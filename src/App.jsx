f842f1ce88a7cd981a62b041bff0bf4c79ca18b5
#GKNavin <gaurav23270143@gmail.com>
2024-05-10 07:00:42 +0530

    css done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AgeCal from './components/AgeCal'
 
 const App = () => {
   return (
-    <div className='w-[100vw] h-[100vh] flex justify-center items-center '>
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#f0f0f0] '>
       <AgeCal/>
     </div>
   )
--- src/components/AgeCal.jsx
+++ src/components/AgeCal.jsx
@@ -3,37 +3,40 @@ import Arrow from '../assets/icon-arrow.svg'
 
 const AgeCal = () => {
   return (
-    <div>
-        <div>
-            <div className="div1">
-                    <div>
-                        <label htmlFor="day">DAY</label>
-                        <input type="number" name='day' />
+    <div className='bg-[#ffffff] p-[2rem] rounded-br-[10rem] '>
+        <div className=' rounded-br-[9rem] flex flex-col gap-[0.2rem]' >
+            <div className="div1 flex gap-[2.5rem] ">
+                    <div className='flex flex-col  items-start w-[8rem]   ' >
+                        <label htmlFor="day" className='mb-[0.3rem] font-semibold ' >DAY</label>
+                        <input type="number" name='day' className='box text-[2.7rem] font-bold pl-[0.5rem] border border-[0.1rem]  border-[#dbdbdb]  ' placeholder='DD' />
+                        <i className='text-[0.9rem]' >This field is required</i>
                     </div>
-                    <div>
-                        <label htmlFor="month">MONTH</label>
-                        <input type="number" name='month' />
+                    <div className='flex flex-col   items-start w-[10rem] ' >
+                        <label htmlFor="month" className='mb-[0.3rem] font-semibold ' >MONTH</label>
+                        <input type="number" name='month' className='box text-[2.7rem] font-bold pl-[0.5rem] border border-[0.1rem]  border-[#dbdbdb]' placeholder='MM'  />
+                        <i className='text-[0.9rem]' >This field is required</i>
                     </div>
-                    <div>
-                        <label htmlFor="year">YEAR</label>
-                        <input type="number" name='year' />
+                    <div className='flex flex-col  items-start w-[8rem] ' >
+                        <label htmlFor="year" className='mb-[0.3rem] font-semibold ' >YEAR</label>
+                        <input type="number" name='year' className='box text-[2.7rem] font-bold pl-[0.5rem] border border-[0.1rem]  border-[#dbdbdb] ' placeholder='YYYY'  />
+                        <i className='text-[0.9rem]' >This field is required</i>
                     </div>
             </div>
-            <div className="div2">
-                <hr />
-                <button><img src={Arrow} alt="" /></button>
+            <div className="div2 flex items-center ">
+                <hr className='bg-[#dbdbdb] w-[30rem] h-[0.1rem] border-[#dbdbdb] ' />
+                <button className='bg-[#854dff] rounded-[7rem] w-[4rem] h-[4rem] p-[0.9rem] flex items-center justify-center ' ><img src={Arrow} alt=""  /></button>
             </div>
-            <div className="div3">
-                <span>- -</span>
-                <h1>years</h1>
+            <div className="div3 flex gap-[1rem] items-center leading-[5rem] ">
+                <span className='text-[5rem] font-extrabold italic text-[#854dff] ' >- -</span>
+                <h1 className='text-[5rem] font-bold  italic'  >years</h1>
             </div>
-            <div className="div4">
-                <span>- -</span>
-                <h1>months</h1>
+            <div className="div3 flex gap-[1rem] items-center leading-[5rem] ">
+                <span className='text-[5rem] font-extrabold  italic text-[#854dff]' >- -</span>
+                <h1 className='text-[5rem] font-bold  italic'  >months</h1>
             </div>
-            <div className="div5">
-                <span>- -</span>
-                <h1>days</h1>
+            <div className="div3 flex gap-[1rem] items-center leading-[5rem] ">
+                <span className='text-[5rem] font-extrabold italic text-[#854dff]' >- -</span>
+                <h1 className='text-[5rem] font-bold  italic'  >days</h1>
             </div>
         </div>
     </div>
